6be94ed0f0b73e683d1291bde8456cb4d79d3b69
mzfirozuddin <mz.firozuddin@gmail.com>
2023-11-25 17:01:28 +0530

    add sign in component

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Button from "./Button";
 
 export { 
     Header, 
     Footer, 
     Logo, 
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    Button
 };
